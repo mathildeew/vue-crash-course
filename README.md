4407de7559817d38a518d2b66014c9ece772b32c
Mathilde Wiik <mathildewiik@hotmail.com>
2025-01-19 15:18:10 +0100

    crash course

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vue-crash-course
+
+This app was built following [this tutorial](https://www.youtube.com/watch?v=VeNfHj6MhgA&t=1429s)
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import './assets/main.css'
+// Imports
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Toast from "vue-toastification";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// Styles
+import "./assets/main.css";
+import "primeicons/primeicons.css";
+import "vue-toastification/dist/index.css";
 
-createApp(App).mount('#app')
+// Create and configure app
+const app = createApp(App);
+
+app.use(router);
+app.use(Toast);
+
+// Mount app to DOM
+app.mount("#app");
